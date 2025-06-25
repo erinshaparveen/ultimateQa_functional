053751dacaeef67e1ec6af2c7066460eb5111510
INSHA <er.inshaParveen@gmail.com>
2025-06-25 23:25:39 +0530

    Add some common method defined in the Util file and added some wait

--- src/main/java/utils/Utils.java
+++ src/main/java/utils/Utils.java
@@ -2,6 +2,7 @@ package utils;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 public class Utils {
 
@@ -11,5 +12,42 @@ public class Utils {
         driver.findElement(locator).click();
     }
 
+    // Check if element is displayed
+    public static boolean isDisplayed(WebDriver driver, String locatorKey) {
+        By locator = LocatorReader.get(locatorKey);
+        return driver.findElement(locator).isDisplayed();
+    }
+
+    // Check if element is present in the DOM
+    public static boolean isPresent(WebDriver driver, String locatorKey) {
+        By locator = LocatorReader.get(locatorKey);
+        return !driver.findElements(locator).isEmpty();
+    }
+
+    // Check if element is enabled
+    public static boolean isEnabled(WebDriver driver, String locatorKey) {
+        By locator = LocatorReader.get(locatorKey);
+        return driver.findElement(locator).isEnabled();
+    }
+
+    // Get text of an element
+    public static String getText(WebDriver driver, String locatorKey) {
+        By locator = LocatorReader.get(locatorKey);
+        return driver.findElement(locator).getText();
+    }
+
+    // Send keys to an input field
+    public static void typeText(WebDriver driver, String locatorKey, String text) {
+        By locator = LocatorReader.get(locatorKey);
+        WebElement element = driver.findElement(locator);
+        element.clear();
+        element.sendKeys(text);
+    }
+
+    // Check if element is selected (for checkbox/radio)
+    public static boolean isSelected(WebDriver driver, String locatorKey) {
+        By locator = LocatorReader.get(locatorKey);
+        return driver.findElement(locator).isSelected();
+    }
 
 }
--- src/main/java/utils/Wait.java
+++ src/main/java/utils/Wait.java
@@ -1,4 +1,46 @@
 package utils;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import java.time.Duration;
 
 public class Wait {
+    private static final int DEFAULT_TIMEOUT = 20;
+
+    // Wait for visibility of element
+    public static WebElement waitForVisibility(WebDriver driver, String locatorKey) {
+        By locator = LocatorReader.get(locatorKey);
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(DEFAULT_TIMEOUT));
+        return wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
+    }
+
+    // Wait for element to be clickable
+    public static WebElement waitForClickability(WebDriver driver, String locatorKey) {
+        By locator = LocatorReader.get(locatorKey);
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(DEFAULT_TIMEOUT));
+        return wait.until(ExpectedConditions.elementToBeClickable(locator));
+    }
+
+    // Wait for presence of element in DOM
+    public static WebElement waitForPresence(WebDriver driver, String locatorKey) {
+        By locator = LocatorReader.get(locatorKey);
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(DEFAULT_TIMEOUT));
+        return wait.until(ExpectedConditions.presenceOfElementLocated(locator));
+    }
+
+    // Wait for element to be invisible
+    public static boolean waitForInvisibility(WebDriver driver, String locatorKey) {
+        By locator = LocatorReader.get(locatorKey);
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(DEFAULT_TIMEOUT));
+        return wait.until(ExpectedConditions.invisibilityOfElementLocated(locator));
+    }
+
+    // Wait for text to be present in element
+    public static boolean waitForTextToBePresent(WebDriver driver, String locatorKey, String text) {
+        By locator = LocatorReader.get(locatorKey);
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(DEFAULT_TIMEOUT));
+        return wait.until(ExpectedConditions.textToBePresentInElementLocated(locator, text));
+    }
 }
